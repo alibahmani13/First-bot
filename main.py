9a87444a0b6768514e26435facec70071e4f1883
Ali <aliking.aking.ak@gmail.com>
2025-05-23 00:14:09 +0200

    new

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from flask import Flask, Response
+from threading import Thread
 import logging
-logging.basicConfig(format='%(name)s - %(asctime)s - %(levelname)s',
+import os
+
+logging.basicConfig(format='%(name)s - %(asctime)s - %(levelname)s ',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-API_TOKEN = "7429159046:AAHpSDjy2lv2b-vaAqneLrkCCnvk3qmKImU"
-GOLDAPI_KEY = "goldapi-3v4m1smawx0q3m-io"
 
-# تابع گرفتن قیمت از goldapi
+API_TOKEN = "7429159046:AAGYuqgAt0VE9vf9rPmuM-Hwpq-Qwut0z0g"
+GOLDAPI_KEY = "goldapi-3v4m1smawx0q3m-io"
 
 
 def get_price(metal_code):
@@ -24,13 +27,10 @@ def get_price(metal_code):
     else:
         return None
 
-# هندلر دستور /start
-
 
 async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! قیمت چی رو می‌خوای؟\nبنویس: طلا یا نقره")
-
-# هندلر پیام‌های متنی برای طلا و نقره
+    await update.message.reply_text(
+        "سلام! قیمت چی رو می‌خوای؟\nبنویس: طلا یا نقره")
 
 
 async def metal_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -50,11 +50,35 @@ async def metal_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await update.message.reply_text("لطفاً فقط بنویس: طلا یا نقره.")
 
-# شروع ربات و ثبت هندلرها
+
+flask_app = Flask('')
+
+
+@flask_app.route('/')
+def home():
+    return "ربات فعال است!"
+
+
+@flask_app.route('/', methods=['HEAD'])
+def head():
+    return Response(status=200)
+
+
+def run():
+    port = int(os.environ.get('PORT', 8080))
+    flask_app.run(host='0.0.0.0', port=port)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+
 if __name__ == '__main__':
+    keep_alive()
     app = ApplicationBuilder().token(API_TOKEN).build()
     app.add_handler(CommandHandler("start", start_handler))
-    app.add_handler(MessageHandler(
-        filters.TEXT & (~filters.COMMAND), metal_handler))
-
+    app.add_handler(
+        MessageHandler(filters.TEXT & (~filters.COMMAND), metal_handler))
     app.run_polling()
+    print("Bot is working")
